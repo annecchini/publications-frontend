cefffd2ab0a8d2e172c4bfd3a36fc4ab01e06ebb
Israel <israelhenrique@protonmail.com>
2018-05-24 17:20:51 -0300

    add props for Input

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const Input = ({
-  type, id, placeHolder, label,
+  name, type, id, placeHolder, label, onChange,
 }) => (<React.Fragment>
   {label && (<label htmlFor={id} >{label}</label>)}
-  <input type={type} className="form-control" id={id} placeholder={placeHolder} />
+  <input name={name} type={type} className="form-control" id={id} placeholder={placeHolder} onChange={onChange} />
 </React.Fragment>)
 
-export const Text = ({ id, placeHolder, label }) => (<Input type="text" id={id} placeHolder={placeHolder} label={label} />)
+export const Text = ({
+  name, id, placeHolder, label, onChange,
+}) => (<Input name={name} type="text" id={id} placeHolder={placeHolder} label={label} onChange={onChange} />)
 
-export const Password = ({ id, placeHolder, label }) => (<Input type="password" id={id} placeHolder={placeHolder} label={label} />)
+export const Password = ({
+  name, id, placeHolder, label, onChange,
+}) => (<Input name={name} type="password" id={id} placeHolder={placeHolder} label={label} onChange={onChange} />)
--- src/components/input/input.test.js
+++ src/components/input/input.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import Input from '.'
+
+const mockCallBack = jest.fn()
 
 describe('<Input.Text /> component', () => {
   let inputText
 
   const inputTextProps = {
-    type: 'text', id: 'text', placeHolder: 'Text', label: 'Text',
+    name: 'name', type: 'text', id: 'text', placeHolder: 'Text', label: 'Text', onChange: mockCallBack,
   }
 
   beforeAll(() => {
@@ -16,6 +18,9 @@ describe('<Input.Text /> component', () => {
 
   test('should be defined', () => {
     expect(inputText.exists()).toBeTruthy()
+  })
+  test('should have name property', () => {
+    expect(inputText.find({ name: inputTextProps.name })).toHaveLength(1)
   })
   test('should have type property', () => {
     expect(inputText.find({ type: inputTextProps.type })).toHaveLength(1)
@@ -29,13 +34,20 @@ describe('<Input.Text /> component', () => {
   test('should have label property', () => {
     expect(inputText.find({ label: inputTextProps.label })).toHaveLength(1)
   })
+  test('test change event', () => {
+    const input = inputText.find({ type: inputTextProps.type })
+    expect(input).toHaveLength(1)
+    expect(input.dive().find('input')).toHaveLength(1)
+    input.dive().find('input').simulate('change')
+    expect(mockCallBack).toHaveBeenCalled()
+  })
 })
 
 describe('<Input.Password /> component', () => {
   let inputPassword
 
   const inputPasswordProps = {
-    type: 'password', id: 'password', placeHolder: 'Password', label: 'Password',
+    name: 'password', type: 'password', id: 'password', placeHolder: 'Password', label: 'Password',
   }
 
   beforeAll(() => {
@@ -44,6 +56,9 @@ describe('<Input.Password /> component', () => {
 
   test('should be defined', () => {
     expect(inputPassword.exists()).toBeTruthy()
+  })
+  test('should have name property', () => {
+    expect(inputPassword.find({ name: inputPasswordProps.name })).toHaveLength(1)
   })
   test('should have type property', () => {
     expect(inputPassword.find({ type: inputPasswordProps.type })).toHaveLength(1)
@@ -56,5 +71,12 @@ describe('<Input.Password /> component', () => {
   })
   test('should have label property', () => {
     expect(inputPassword.find({ label: inputPasswordProps.label })).toHaveLength(1)
+  })
+  test('test change event', () => {
+    const input = inputPassword.find({ type: inputPasswordProps.type })
+    expect(input).toHaveLength(1)
+    expect(input.dive().find('input')).toHaveLength(1)
+    input.dive().find('input').simulate('change')
+    expect(mockCallBack).toHaveBeenCalled()
   })
 })
