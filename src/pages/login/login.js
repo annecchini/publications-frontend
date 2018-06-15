cca0077bd4a92a1db048a3af053775ff4a801b00
Israel <israelhenrique@protonmail.com>
2018-06-15 17:54:35 -0300

    login page

---
+++ src/pages/login/login.js
@@ -0,0 +1,24 @@
+import React, { Component }  from 'react'
+import Form from '../../components/form'
+
+import { login } from '../../services/login/login'
+
+export class Login extends Component {
+  handleSubmit = (username, password) => {
+    const status = login({ email: username, password })
+    console.log(status)
+  }
+
+  render() {
+    const formProps = {
+      onSubmit: this.handleSubmit,
+      userLabel: 'User',
+      passwordLabel: 'Password',
+    }
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <Form.Login {...formProps} />
+      </div>
+    )
+  }
+}
---
+++ src/pages/login/login.story.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Login from '.'
+
+storiesOf('Login page', module)
+  .add('Login page', () => <Login  />)
